a4c3509d5db00f63d28723ec55fb28506aba7f69
hb5021 <noel86@naver.com>
2017-02-10 16:08:36 +0900

    eslint add commit

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,209 @@
 define([
 	"bootstrap",
 ], function() {
+	var popupCssSelector = "";
+
+	function closeLayerPopup() {
+		$(popupCssSelector).hide();
+
+		$(".block-layer").remove();
+		$("body").css("overflow", "");
+
+		popupCssSelector = "";
+	}
+	function openAjaxPopup(layerName) {
+		$.ajax({
+			url: "/layers/" + layerName + ".html",
+			success: function(html) {
+				$("body").css("overflow", "hidden");
+
+				var blockLayerHTML = "<div class='block-layer ajax'></div>";
+				$("body").append(blockLayerHTML);
+
+				$("body").append(html);
+
+				$("#sign-btn-container>input:first-child").focus();
+
+				attachPopupEvents(layerName);
+			},
+		});
+	}
+	function closeAjaxPopup() {
+		$(".block-layer.ajax").remove();
+
+		if ($(".block-layer").length === 0) {
+			$("body").css("overflow", "");
+		}
+
+		$("#sign-box").remove();
+	}
+	function attachPopupEvents(layerName) {
+		if (layerName === "sign-up") {
+			$("#member-sign-up").on("click", function() {
+				signUp();
+			});
+
+			$("#reset").on("click", function() {
+				$("#user-id").val("");
+				$("#user-password").val("");
+				$("#user-password-crm").val("");
+
+				$("#user-id").focus();
+			});
+		}
+		else if (layerName === "sign-in") {
+			$("#hp-member-sign-in").on("click", function() {
+				signIn();
+			});
+		}
+		else if (layerName === "member-info") {
+			$("#hp-member-info-update").on("click", function() {
+				updateMemberInfo();
+			});
+
+			$(".hp-reset").on("click", function() {
+				$("#hp-user-pw").val("");
+				$("#hp-user-pw-cfm").val("");
+
+				$("#hp-user-pw").focus();
+			});
+		}
+
+		$(".block-layer.ajax, .sign-exit").on("click", function() {
+			closeAjaxPopup();
+		});
+	}
+
+	attachPopupEvents();
+
+	function signUp() {
+		var userId = $("#user-id").val();
+		var userPw = $("#user-password").val();
+		var userPwCfm = $("#user-password-crm").val();
+
+		if (userId === undefined || userId === "") {
+			alert("아이디를 입력하세요.");
+			$("#user-id").focus();
+			return;
+		}
+		else if (userPw === undefined || userPw === "") {
+			alert("비밀번호를 입력하세요.");
+			$("#user-password").focus();
+			return;
+		}
+		else if (userPw !== userPwCfm) {
+			alert("비밀번호 확인을 동일하게 입력하세요.");
+			$("#user-password-crm").focus();
+			return;
+		}
+
+		$.ajax({
+			url: global.root + "/api2/member/signup",
+			method: "POST",
+			data: {
+				userId: userId,
+				userPw: userPw,
+			},
+			success: function(data) {
+				if (data.result === "ok") {
+					alert(userId + "님 환영합니다.");
+					closeAjaxPopup();
+				}
+				else {
+					alert("정상적으로 가입되지 않았습니다.");
+				}
+			},
+			error: function(jqXHR) {
+				alert(jqXHR.responseJSON.message);
+			},
+		});
+	}
+
+	function signIn() {
+		var userId = $("#user-id").val();
+		var userPw = $("#user-password").val();
+
+		if (userId === undefined || userId === "") {
+			alert("아이디를 입력하세요.");
+			$("#user-id").focus();
+			return;
+		}
+		else if (userPw === undefined || userPw === "") {
+			alert("비밀번호를 입력하세요.");
+			$("#user-pw").focus();
+			return;
+		}
+
+		$.ajax({
+			url: global.root + "/api2/member/signin",
+			method: "POST",
+			data: {
+				userId: userId,
+				userPw: userPw,
+			},
+			success: function(data) {
+				if (data.result === "ok") {
+					alert(userId + "님 환영합니다.");
+					closeAjaxPopup();
+
+					$(".hp-sign-up").hide();
+					$(".hp-sign-in").hide();
+					$(".hp-member-info").show();
+					$(".hp-sign-out").show();
+				}
+				else {
+					alert("정상적으로 로그인되지 않았습니다.");
+				}
+			},
+			error: function(jqXHR) {
+				alert(jqXHR.responseJSON.message);
+			},
+		});
+	}
+
+	function updateMemberInfo() {
+		var userPw = $("#hp-user-pw").val();
+		var userPwCfm = $("#hp-user-pw-cfm").val();
+
+		if (userPw === undefined || userPw === "") {
+			alert("비밀번호를 입력하세요.");
+			$("#hp-user-pw").focus();
+			return;
+		}
+		else if (userPw !== userPwCfm) {
+			alert("비밀번호 확인을 동일하게 입력하세요.");
+			$("#hp-user-pw-cfm").focus();
+			return;
+		}
+
+		$.ajax({
+			url: global.root + "/api2/member/update",
+			method: "POST",
+			data: {
+				userPw: userPw,
+			},
+			success: function(data) {
+				if (data.result === "ok") {
+					alert("정상적으로 수정되었습니다.");
+					closeAjaxPopup();
+				}
+				else {
+					alert("정상적으로 수정되지 않았습니다.");
+				}
+			},
+			error: function(jqXHR) {
+				alert(jqXHR.responseJSON.message);
+			},
+		});
+	}
+
+	$("#sign-up-btn").on("click", function() {
+		openAjaxPopup("sign-up");
+		hideloginLayer();
+	});
+	$(".block-layer").on("click", function() {
+		closeLayerPopup();
+	});
 	$("#header-logo, #scrolled-logo").on("click", function() {
 		location.href = "/";
 	});
@@ -83,7 +286,6 @@ define([
 		event.stopPropagation();
 		hideloginLayer();
 	});
-
 	$("#id-btn, #password-btn").on("click", function() {
 		clearKeywords();
 	});
