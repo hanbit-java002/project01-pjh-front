b6a4845ce0deda28bbe44232a674e8594ec0c8d0
hb5021 <noel86@naver.com>
2017-03-22 20:01:51 +0900

    add commit

--- src/js/public-list.js
+++ src/js/public-list.js
@@ -1,5 +1,18 @@
 require([
 	"common",
 ], function() {
-
+	/* function publicInfo() {
+		var windowWidth = $( window ).width();
+		if(windowWidth < 1440) {
+			$(".public-info").css("position", "relative");
+			$(".public-info").css("width", "100%");
+			$(".public-info").css("display", "inline-block");
+		}
+		else {
+			$(".public-info").css("position", "absolute");
+			$(".public-info").css("width", "31%");
+			$(".public-info").css("display", "block");
+		}
+	};
+	publicInfo();*/
 });
